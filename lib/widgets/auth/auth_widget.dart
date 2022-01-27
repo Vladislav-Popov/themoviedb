13d7fb17922807f3633a0ddd62e762b1a16f5c13
Vladislav <57004611+Vladislav-Popov@users.noreply.github.com>
2022-01-27 06:31:57 +0300

    Continue

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -27,17 +27,92 @@ class _HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textStyle = const TextStyle(
+      fontSize: 16,
+      color: Colors.black,
+    );
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
           SizedBox(height: 25),
-          Text(
-              'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.'),
+          _FormWidget(),
           SizedBox(height: 25),
-          Text('If you signed up but didn`t get your verification email'),
+          Text(
+            'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.',
+            style: textStyle,
+          ),
+          SizedBox(height: 25),
+          Text('If you signed up but didn`t get your verification email',
+              style: textStyle),
         ],
       ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget({Key? key}) : super(key: key);
+
+  @override
+  _FormWidgetState createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<_FormWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = const TextStyle(
+      fontSize: 16,
+      color: Color(0xFF212529),
+    );
+    final color = Color(0xFF01B4E4);
+    final textFieldDecoration = const InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      isCollapsed: true,
+    );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Username', style: textStyle),
+        SizedBox(height: 5),
+        TextField(
+          decoration: textFieldDecoration,
+        ),
+        SizedBox(height: 20),
+        Text('Password', style: textStyle),
+        SizedBox(height: 5),
+        TextField(
+          decoration: textFieldDecoration,
+          obscureText: true,
+        ),
+        SizedBox(height: 5),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Login'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(color),
+                textStyle: MaterialStateProperty.all(
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                padding: MaterialStateProperty.all(
+                  EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                ),
+              ),
+            ),
+            SizedBox(width: 10),
+            TextButton(
+              onPressed: () {},
+              child: Text('Reset password'),
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(color),
+                  textStyle: MaterialStateProperty.all(
+                      TextStyle(fontSize: 16, fontWeight: FontWeight.w400))),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
