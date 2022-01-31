756663e363dbb4dfed9345823edfe376137fd4df
Vladislav <57004611+Vladislav-Popov@users.noreply.github.com>
2022-01-31 19:20:23 +0300

    Работа с изображениями

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,7 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+
+  static const String vechnie = 'images/vechnie.jpg';
+}
--- lib/widgets/main_screen/main_screen_widget.dart
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:themoviedb/widgets/movie_list/movie_list_widget.dart';
 
 class MainScreenWidget extends StatefulWidget {
   const MainScreenWidget({Key? key}) : super(key: key);
@@ -13,7 +14,7 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
 
   static const List<Widget> _widgetOptions = <Widget>[
     Text('Home'),
-    Text('Movie'),
+    MovieListWidget(),
     Text('TV'),
   ];
 
