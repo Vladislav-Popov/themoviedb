cf2e79337245c0667e96ac9150052b9cf7b6eff1
Vladislav <57004611+Vladislav-Popov@users.noreply.github.com>
2022-02-08 08:25:06 +0300

    #41 start

---
+++ lib/widgets/movie_details/movie_details_info_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class MovieDetailsInfoWidget extends StatelessWidget {
+  const MovieDetailsInfoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('info widget'),
+    );
+  }
+}
