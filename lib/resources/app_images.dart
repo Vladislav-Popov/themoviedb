d18d38aa508e56031f524b935871aecdb734c9ae
Vladislav <57004611+Vladislav-Popov@users.noreply.github.com>
2022-02-09 07:17:35 +0300

    #41 done

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -4,4 +4,6 @@ class AppImages {
   AppImages._();
 
   static const String moviePlacholder = 'images/vechnie.jpg';
+  static const String topHeader = 'images/topHeader.png';
+  static const String topHeaderSubImage = 'images/topHeaderSubImage.jpeg';
 }
